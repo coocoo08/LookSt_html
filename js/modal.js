ae60b6d450cd1176e671fa5e92e874788ce13019
jjhyeonjj <jjh4523a@gmail.com>
2022-10-23 01:19:03 +0900

    프로필 팔로워/팔로잉 리스트 모달창 변경

--- js/modal.js
+++ js/modal.js
@@ -1,45 +1,40 @@
-// const loremIpsum = document.getElementById("lorem-ipsum")
-
-// fetch("https://baconipsum.com/api/?type=all-meat&paras=200&format=html")
-//     .then(response => response.text())
-//     .then(result => loremIpsum.innerHTML = result)
-
-// const modal = document.getElementById("modal")
-
-function modalOn() {
-    modal.style.display = "flex"
-    document.body.style.overflow = "hidden";
+// Modal을 가져옵니다.
+var modals = document.getElementsByClassName("modal");
+// Modal을 띄우는 클래스 이름을 가져옵니다.
+var btns = document.getElementsByClassName("btn");
+// Modal을 닫는 close 클래스를 가져옵니다.
+var spanes = document.getElementsByClassName("close");
+var funcs = [];
+ 
+// Modal을 띄우고 닫는 클릭 이벤트를 정의한 함수
+function Modal(num) {
+  return function() {
+    // 해당 클래스의 내용을 클릭하면 Modal을 띄웁니다.
+    btns[num].onclick =  function() {
+        modals[num].style.display = "block";
+        console.log(num);
+    };
+ 
+    // <span> 태그(X 버튼)를 클릭하면 Modal이 닫습니다.
+    spanes[num].onclick = function() {
+        modals[num].style.display = "none";
+    };
+  };
 }
-
-function isModalOn() {
-    return modal.style.display === "flex"
+ 
+// 원하는 Modal 수만큼 Modal 함수를 호출해서 funcs 함수에 정의합니다.
+for(var i = 0; i < btns.length; i++) {
+  funcs[i] = Modal(i);
 }
-
-function modalOff() {
-    modal.style.display = "none"
-    document.body.style.overflow = "unset";
+ 
+// 원하는 Modal 수만큼 funcs 함수를 호출합니다.
+for(var j = 0; j < btns.length; j++) {
+  funcs[j]();
 }
-
-
-const btnModal = document.getElementById("btn-modal")
-btnModal.addEventListener("click", e => {
-    modalOn()
-})
-
-const closeBtn = modal.querySelector(".close-area")
-closeBtn.addEventListener("click", e => {
-    modalOff()
-})
-
-modal.addEventListener("click", e => {
-    const evTarget = e.target
-    if(evTarget.classList.contains("modal-overlay")) {
-        modalOff()
-    }
-})
-
-window.addEventListener("keyup", e => {
-    if(isModalOn() && e.key === "Escape") {
-        modalOff()
-    }
-})
+ 
+// Modal 영역 밖을 클릭하면 Modal을 닫습니다.
+window.onclick = function(event) {
+  if (event.target.className == "modal") {
+      event.target.style.display = "none";
+  }
+};
