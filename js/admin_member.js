3f27711e6f284e51d679011c8741d6b39d3364b1
Cat Hanbit <futurely08@naver.com>
2022-10-26 20:00:07 +0900

    admin modal 추가 중간 저장

--- js/admin_member.js
+++ js/admin_member.js
@@ -1,16 +1,16 @@
 // Modal을 가져옵니다.
-var modals = document.getElementsByClassName("modal");
+let modals = document.getElementsByClassName("modal");
 // Modal을 띄우는 클래스 이름을 가져옵니다.
-var btns = document.getElementsByClassName("btn");
+let modalbtns = document.getElementsByClassName("modalbtn");
 // Modal을 닫는 close 클래스를 가져옵니다.
-var spanes = document.getElementsByClassName("close");
-var funcs = [];
+let spanes = document.getElementsByClassName("close");
+let funcs = [];
  
 // Modal을 띄우고 닫는 클릭 이벤트를 정의한 함수
 function Modal(num) {
   return function() {
     // 해당 클래스의 내용을 클릭하면 Modal을 띄웁니다.
-    btns[num].onclick =  function() {
+    modalbtns[num].onclick =  function() {
         modals[num].style.display = "block";
         console.log(num);
     };
@@ -23,12 +23,12 @@ function Modal(num) {
 }
  
 // 원하는 Modal 수만큼 Modal 함수를 호출해서 funcs 함수에 정의합니다.
-for(var i = 0; i < btns.length; i++) {
+for(var i = 0; i < modalbtns.length; i++) {
   funcs[i] = Modal(i);
 }
  
 // 원하는 Modal 수만큼 funcs 함수를 호출합니다.
-for(var j = 0; j < btns.length; j++) {
+for(var j = 0; j < modalbtns.length; j++) {
   funcs[j]();
 }
  
